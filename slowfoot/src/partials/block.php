1311d2353fd548627a9c0165f1497f2303cdf67a
rw <rw@20sec.net>
2026-04-15 01:31:15 +0200

    cleanup

--- slowfoot/src/partials/block.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-?>
-  <?=$sanity_text($body)?>
-
--- slowfoot/web-deploy/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define('SLOWFOOT_START', microtime(true));
-define('SLOWFOOT_BASE', dirname(__DIR__));
-
-require SLOWFOOT_BASE . '/vendor/autoload.php';
-
-require SLOWFOOT_BASE . '/vendor/cwmoss/slowfoot-lib/src/webdeploy.php';
